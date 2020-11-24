af142d642dd024cb53bee432ddc3ac5fa94eeae8
Filippo Cavallari <filippo.cavallari99@gmail.com>
2020-11-24 21:05:03 +0100

    Fixed null plugin instance

--- src/main/java/it/forgottenworld/fwtownymisc/fwtownymisc/FWTownyMisc.java
+++ src/main/java/it/forgottenworld/fwtownymisc/fwtownymisc/FWTownyMisc.java
@@ -17,6 +17,7 @@ public final class FWTownyMisc extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        INSTANCE = this;
         registerListeners();
         registerCommands();
     }
